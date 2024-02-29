e33bd92891e7f316cd7456830a3a0a95be416a3e
Austin Graham <austiingraham@gmail.com>
2024-02-29 18:22:48 -0500

    added Header component with portfolio navigation

---
+++ src/components/Header.jsx
@@ -0,0 +1,35 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const Header = () => {
+  return (
+    <header>
+      <nav className='navbar navbar-expand-sm navbar-dark bg-dark'>
+        <div className='container-fluid'>
+          <a className='navbar-brand' href='/'>Austin</a>
+          <button className='navbar-toggler' type='button' data-bs-toggle='collapse' data-bs-target='#navbarNav'
+            aria-controls='navbarNav' aria-expanded='false' aria-label='Toggle navigation'>
+            <span className='navbar-toggler-icon'></span>
+          </button>
+          <div className='collapse navbar-collapse' id='navbarNav'>
+            <ul className='navbar-nav ms-auto'>
+              <li className='nav-item'>
+                <a className='nav-link' href='/'>About Me</a>
+              </li>
+              <li className='nav-item'>
+                <a className='nav-link' href='/portfolio'>Portfolio</a>
+              </li>
+              <li className='nav-item'>
+                <a className='nav-link' href='/contact'>Contact</a>
+              </li>
+              <li className='nav-item'>
+                <a className='nav-link' href='/resume'>Resume</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
