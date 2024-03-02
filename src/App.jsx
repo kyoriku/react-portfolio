e5b34dac9f313019dd7560f8be573d65d855c91d
Austin Graham <austiingraham@gmail.com>
2024-03-02 03:21:56 -0500

    added Footer component with social links, added styling to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import Header from './components/Header';
+import Footer from './components/Footer';
+import './styles/App.css';
 
 function App() {
   return (
-    <>
+    <main className='app-container'>
       <Header />
-    </>
-  )
+      <Footer />
+    </main>
+  );
 }
 
 export default App
---
+++ src/components/Footer.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="bg-dark mt-auto">
+      <section className="navbar navbar-expand-sm navbar-dark justify-content-center">
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <a className="nav-link" href="https://github.com/kyoriku">GitHub</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="https://www.linkedin.com/in/austingraham1/">LinkedIn</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="https://www.youtube.com/@arcaneviva">YouTube</a>
+          </li>
+        </ul>
+      </section>
+    </footer>
+  );
+}
+
+export default Footer;
