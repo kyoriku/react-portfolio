4da0fb61abd0376388c9cd43dec42d0d0949c630
Austin Graham <austiingraham@gmail.com>
2024-03-02 03:40:02 -0500

    moved bootstrap import to App.jsx for global accessibility

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Header from './components/Header';
 import Footer from './components/Footer';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles/App.css';
 
 function App() {
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,13 +6,13 @@ const Footer = () => {
       <div className="navbar navbar-expand-sm navbar-dark justify-content-center">
         <ul className="navbar-nav">
           <li className="nav-item">
-            <a className="nav-link" href="https://github.com/kyoriku">GitHub</a>
+            <a className="nav-link" href="https://github.com/kyoriku" target="_blank">GitHub</a>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="https://www.linkedin.com/in/austingraham1/">LinkedIn</a>
+            <a className="nav-link" href="https://www.linkedin.com/in/austingraham1/" target="_blank">LinkedIn</a>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="https://www.youtube.com/@arcaneviva">YouTube</a>
+            <a className="nav-link" href="https://www.youtube.com/@arcaneviva" target="_blank">YouTube</a>
           </li>
         </ul>
       </div>
