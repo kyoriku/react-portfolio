ba7ebd9832323357511926110badbe5ee97ab125
Austin Graham <austiingraham@gmail.com>
2024-03-30 02:21:42 -0400

    added more projects

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -2,16 +2,31 @@ import React from 'react'; // Importing React to use JSX
 import Project from './Project'; // Importing Project component to display project information
 
 // Importing images to display project information
+import AdminAccess from '../assets/images/adminaccess.jpg';
+import BookSearch from '../assets/images/book-search-engine.jpg';
 import TextEditor from '../assets/images/text-editor.jpg';
 import Rendezview from '../assets/images/rendezview.jpg';
 import TechBlog from '../assets/images/tech-blog-cms.jpg';
 import FilmFinder from '../assets/images/film-finder.jpg';
 import WeatherDashboard from '../assets/images/weather-dashboard.jpg';
 import CodeQuiz from '../assets/images/coding-quiz.jpg';
+import HTMLPortfolio from '../assets/images/portfolio.jpg';
 
 // Functional component to display portfolio
 const Portfolio = () => {
   const projects = [
+    {
+      title: 'AdminAccess',
+      image: AdminAccess,
+      deployedLink: 'https://adminaccess-f697b23e85fa.herokuapp.com/',
+      githubLink: 'https://github.com/kyoriku/AdminAccess',
+    },
+    {
+      title: 'Book Search Engine',
+      image: BookSearch,
+      deployedLink: 'https://mern-book-search-engine-1asm.onrender.com/',
+      githubLink: 'https://github.com/kyoriku/book-search-engine',
+    },
     {
       title: 'Text Editor',
       image: TextEditor,
@@ -47,6 +62,12 @@ const Portfolio = () => {
       image: CodeQuiz,
       deployedLink: 'https://kyoriku.github.io/code-quiz/',
       githubLink: 'https://github.com/kyoriku/code-quiz',
+    },
+    {
+      title: 'HTML Portfolio',
+      image: HTMLPortfolio,
+      deployedLink: 'https://kyoriku.github.io/portfolio/',
+      githubLink: 'https://github.com/kyoriku/portfolio',
     },
   ];
 
