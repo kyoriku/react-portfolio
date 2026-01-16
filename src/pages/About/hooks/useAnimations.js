23d1be022d0f9a735e16f450127cd619cb9ed3cb
kyoriku <austiingraham@gmail.com>
2026-01-16 13:32:07 -0500

    removed unused files

--- src/pages/About/hooks/useAnimations.js
+++
@@ -1,7 +0,0 @@
-import { useReducedMotion } from 'framer-motion';
-import { animationConfig } from '../config/animations';
-
-export const useAnimations = () => {
-  const shouldReduceMotion = useReducedMotion();
-  return shouldReduceMotion ? {} : animationConfig;
-};
